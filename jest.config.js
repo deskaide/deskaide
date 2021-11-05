e0bbad98f4c1af29a2d5293bc059df8b357b0782
sh4hids <dev@shahid.pro>
2021-11-05 11:17:06 +0600

    ♻ refactor test setup

--- jest.config.js
+++ jest.config.js
@@ -4,7 +4,7 @@ module.exports = {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   testMatch: [
     '<rootDir>/packages/renderer/**/__tests__/**/*.{js,jsx,ts,tsx}',
-    '<rootDir>/packages/renderer/**/*.{spec,test}.{ts,tsx}',
+    '<rootDir>/packages/renderer/**/*.{spec,test}.{js,jsx,ts,tsx}',
   ],
   testEnvironment: 'jest-environment-jsdom',
 };
