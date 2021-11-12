04fd3007ac54468d2ba7299a5609b9f5c1ff5848
sh4hids <dev@shahid.pro>
2021-11-12 10:43:40 +0600

    ♻ refactor theme & tooltip color

--- packages/renderer/src/styles/themes/baseTheme.ts
+++ packages/renderer/src/styles/themes/baseTheme.ts
@@ -45,7 +45,7 @@ const fontWeights: CustomNumberArrayType = [
 const colors = {
   primary: ['#532FA8', '#6237C6', '#9577D9'],
   dark: ['#191924', '#27283F', '#53556E'],
-  light: ['#E7E7EE', '#F6F6F9', '#FFFFFF'],
+  light: ['#FFFFFF', '#F6F6F9', '#E7E7EE'],
   success: ['#04A660', '#07C270', '#3ADA8A'],
   error: ['#A1140C', '#F25C54', '#F47771'],
   warning: ['#C26C00', '#FF8E00', '#FDA642'],
