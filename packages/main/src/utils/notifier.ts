8d2c5e4531208e9c7112a54cd5fc5515aec3b2b4
Shahidul Islam Majumder <dev@shahid.pro>
2022-07-09 11:39:47 +0600

    feat: add icon in notification

--- packages/main/src/utils/notifier.ts
+++ packages/main/src/utils/notifier.ts
@@ -1,10 +1,12 @@
 import { Notification } from 'electron';
 
 import type { NotificationMessage } from '../../../../types/NotificationMessage';
+import logo from '../../../../assets/icons/icon.png';
 
 export const notify = (message: NotificationMessage) => {
   new Notification({
     title: message.title,
     body: message.body,
+    icon: logo,
   }).show();
 };
