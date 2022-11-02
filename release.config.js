317e3d8cd922153fe63d5b5a8d1036a62cd0c8a2
Shahidul Islam Majumder <dev@shahid.pro>
2022-11-02 07:56:24 +0600

    fix: refactor release config

--- release.config.js
+++ release.config.js
@@ -1,14 +1,4 @@
 module.exports = {
-  branches: ['main', 'chore/cimmitizen'],
-  plugins: [
-    '@semantic-release/commit-analyzer',
-    // '@semantic-release/release-notes-generator',
-    // '@semantic-release/changelog',
-    // [
-    //   '@semantic-release/github',
-    //   {
-    //     assets: ['CHANGELOG.md', 'package.json', 'package-lock.json'],
-    //   },
-    // ],
-  ],
+  branches: ['main'],
+  plugins: ['@semantic-release/commit-analyzer'],
 };
