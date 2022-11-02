6266bb5ec1e7dc0c220e22843d6ce703f641794f
Shahidul Islam Majumder <dev@shahid.pro>
2022-11-02 18:36:02 +0600

    style: fix quote issue

--- packages/renderer/src/styles/themes/light.ts
+++ packages/renderer/src/styles/themes/light.ts
@@ -15,9 +15,9 @@ export const light: ThemeInterface = {
     text2: baseTheme.colors.dark[2],
   },
   elevations: [
-    `0px 0px 1px rgba(39, 40, 63, 0.08), 0px 0.4px 2px rgba(83, 85, 110, 0.16)`,
-    `0px 0px 1px rgba(39, 40, 63, 0.08), 0px 2px 4px rgba(25, 25, 36, 0.16)`,
-    `0px 0px 1px rgba(39, 40, 63, 0.08), 0px 4px 8px rgba(25, 25, 36, 0.16)`,
-    `0px 2px 8px rgba(39, 40, 63, 0.08), 0px 16px 32px rgba(25, 25, 36, 0.16)`,
+    '0px 0px 1px rgba(39, 40, 63, 0.08), 0px 0.4px 2px rgba(83, 85, 110, 0.16)',
+    '0px 0px 1px rgba(39, 40, 63, 0.08), 0px 2px 4px rgba(25, 25, 36, 0.16)',
+    '0px 0px 1px rgba(39, 40, 63, 0.08), 0px 4px 8px rgba(25, 25, 36, 0.16)',
+    '0px 2px 8px rgba(39, 40, 63, 0.08), 0px 16px 32px rgba(25, 25, 36, 0.16)',
   ],
 };
