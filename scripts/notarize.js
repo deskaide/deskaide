ab32198817208988952cec79cf17f3b068c7fe43
Shahidul Islam Majumder <dev@shahid.pro>
2022-11-16 10:58:02 +0600

    ci: fix notarize config

--- scripts/notarize.js
+++ scripts/notarize.js
@@ -9,13 +9,21 @@ exports.default = async function notarizing(context) {
     return;
   }
 
+  if (
+    !process.env.APPLE_ID ||
+    !process.env.APPLE_ID_PASSWORD ||
+    !process.env.APPLE_TEAM_ID
+  ) {
+    return;
+  }
+
   const appName = context.packager.appInfo.productFilename;
 
   return await notarize({
-    tool: 'notarytool',
     appBundleId: 'app.deskaide.www',
     appPath: `${appOutDir}/${appName}.app`,
     appleId: process.env.APPLE_ID,
     appleIdPassword: process.env.APPLE_ID_PASSWORD,
+    ascProvider: process.env.APPLE_TEAM_ID,
   });
 };
