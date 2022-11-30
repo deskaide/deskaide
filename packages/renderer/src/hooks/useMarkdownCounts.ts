240605a373811291ecab0328ae86ddd678714c01
Shahidul Islam Majumder <dev@shahid.pro>
2022-11-30 19:57:34 +0600

    chore: update packages

--- packages/renderer/src/hooks/useMarkdownCounts.ts
+++ packages/renderer/src/hooks/useMarkdownCounts.ts
@@ -1,7 +1,4 @@
 import readingTime from 'reading-time';
-// import english from 'retext-english';
-// import remark2retext from 'remark-retext';
-// import { visit } from 'unist-util-visit';
 
 export const useMarkdownCounts = (markdownDoc: string) => {
   const stats = readingTime(markdownDoc);
